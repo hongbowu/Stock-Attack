ad332eb99ea6aa63eb95de2edbee78910df8c33d
Brandon <brandondenzell566@outlook.com>
2024-02-09 13:16:40 -0500

    made a dashbord jsx and imported react and a dashboard component

---
+++ client/src/pages/dashboard.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Welcome</h2>
+      <div>
+        <h3>Portfolio Summary</h3>
+        <p>Total Investment: </p>
+        <p>Current Value: </p>
+        <p>Performance: </p>
+      </div>
+      <div>
+        <h3>Market News</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
