86b73a3adcbceeb1834c4b0232ec18076f1ed786
Brandon <brandondenzell566@outlook.com>
2024-02-09 13:52:29 -0500

    added a homepage component

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -1,1 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Welcome to Stock Attack!</h1>
+      <p>
+        Your go-to app for tracking stocks and managing your investment
+        portfolio.
+      </p>
+      <div>
+        <Link to="/signup">Sign Up Now</Link>
+      </div>
+    </div>
+  );
+};
