f9def8daa4243f37fcda929cfd47645b35af1988
hongbowu <wuhongbo87@gmail.com>
2024-02-09 12:35:30 -0500

    add simple query users

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,1 +1,11 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
+
+export const QUERY_USER = gql`
+    query users {
+        users {
+            _id
+            username
+            stocks
+        }
+    }
+`;
