ec54a6beca232a94cff1042ad6a6ecebaaaa5890
Jescel James <jesceljames@gmail.com>
2024-02-09 12:31:19 -0500

    added dependencies /requirements in server.js

---
+++ server/server.js
@@ -0,0 +1,5 @@
+// REQUIREMENTS/ DEPENDENCIES_________________________
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
