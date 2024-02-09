134621abc2739989c49439bcb1c1de7324423e58
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-02-09 14:34:54 -0500

    Added env package

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 
-const apiKey = '64tFQ3PBgE2yTRqLdfw80pVoLt5AGIJN'; // Replace 'YOUR_API_KEY' with your actual API key from Polygon.io
-
+// const apiKey = '64tFQ3PBgE2yTRqLdfw80pVoLt5AGIJN'; // Replace 'YOUR_API_KEY' with your actual API key from Polygon.io
+require('dotenv').config()
 // Function to fetch previous day's close price of Apple (AAPL) stock
 async function fetchPreviousClosePrice() {
   try {
-    const response = await fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/prev?adjusted=true&apiKey=${apiKey}`);
+    const response = await fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/prev?adjusted=true&apiKey=${process.env.API_KEY}`);
     const data = await response.json();
     
     if (data.results && data.results.length > 0) {
@@ -19,13 +19,12 @@ async function fetchPreviousClosePrice() {
   }
 }
 
-console.log(fetchPreviousClosePrice())
 
 function saveStockPrice(price) {
     // You can implement your saving logic here, for example:
     // Saving to localStorage
-    localStorage.setItem('apple_previous_close_price', price);
-    console.log('Previous day\'s close price saved:', price);
+   
+    console.log('Previous day\'s close price:', price);
   }
   
   // Call the function to fetch and save the previous day's close price
