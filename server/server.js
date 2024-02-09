00f9daff52696fd697a2cb9b323bd113d23a0e6d
Jescel James <jesceljames@gmail.com>
2024-02-09 12:33:52 -0500

    add schemas and connections

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 // REQUIREMENTS/ DEPENDENCIES_________________________
 const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
+  const { ApolloServer } = require('@apollo/server');
+  const { expressMiddleware } = require('@apollo/server/express4');
+  const path = require('path');
+
+// SCHEMAS AND CONNECTIONS
+  const { typeDefs, resolvers } = require('./schemas');
+  const db = require('./config/connection');
