36c6d3e0b499e7f4fdb0977daae88be29cc3bf51
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-02-12 12:18:38 -0500

    Begin build out of fetchStockData function

---
+++ data2.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+ // Replace 'YOUR_API_KEY' with your actual API key from Polygon.io
+
+// Function to fetch previous day's close price, current stock price, ticker symbol, and company name of Apple (AAPL) stock
+async function fetchStockData() {
+  try {
+    // Fetch previous day's close price and current stock price
+    const prevCloseResponse = await fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/prev?adjusted=true&apiKey=${process.env.API_KEY}`);
+    const prevCloseData = await prevCloseResponse.json();
+    const currentPriceResponse = await fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/${getCurrentDate()}/${getCurrentDate()}?apiKey=${process.env.API_KEY}`);
+    const currentPriceData = await currentPriceResponse.json();
+
+   
