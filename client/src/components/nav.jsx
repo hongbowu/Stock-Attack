91b69ca652f6c7049ebb6abec50e1a2a2fc441fb
Brandon <brandondenzell566@outlook.com>
2024-02-12 11:26:12 -0500

    made component for nav

---
+++ client/src/components/nav.jsx
@@ -0,0 +1,14 @@
+const Nav = () => {
+    return (
+    <nav>
+        {/* logo or home link */}
+        <Link to="/">
+          {/* logo? */}
+        Stock Attack
+        </Link>
+        {/* navigation links */}
+        <Link to="/investing">Investing</Link>
+        <Link to="/about">Account</Link>
+    </nav>
+    );
+};
