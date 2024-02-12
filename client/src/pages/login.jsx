a634f41906b2a957fdc75408ed6558de04e84418
Brandon <brandondenzell566@outlook.com>
2024-02-12 13:57:52 -0500

    imported cssbaseline from mui

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const Login = () => {
     return (
