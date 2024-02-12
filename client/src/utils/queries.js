36f4cf09262f6c7e13dc7aa766f4f3e3f43c5181
hongbowu <wuhongbo87@gmail.com>
2024-02-12 12:06:36 -0500

    add query all users to queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,26 @@
 import { gql } from "@apollo/client";
 
-export const QUERY_USER = gql`
+export const QUERY_ALL_USERS = gql`
     query users {
-        
+        user {
+            _id
+            name
+            email
+            stocks{
+                _id
+                ticker
+                name
+            }
+        }
     }
 `;
+
+// export const QUERY_ALL_STOCKS = gql`
+//     query stocks {
+//         stock{
+//             _id
+//             ticker
+//             name
+//         }
+//     }
+// `;
