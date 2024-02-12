65baefbee3925b683b57e06f4953c2d2f12e694d
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-02-12 14:57:25 -0500

    Delete current price API request

--- data2.js
+++ data2.js
@@ -7,8 +7,7 @@ async function fetchStockData() {
     // Fetch previous day's close price and current stock price
     const prevCloseResponse = await fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/prev?adjusted=true&apiKey=${process.env.API_KEY}`);
     const prevCloseData = await prevCloseResponse.json();
-    const currentPriceResponse = await fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/${getCurrentDate()}/${getCurrentDate()}?apiKey=${process.env.API_KEY}`);
-    const currentPriceData = await currentPriceResponse.json();
+   
 
     // Fetch ticker symbol and company name
     const infoResponse = await fetch(`https://api.polygon.io/v1/meta/symbols/AAPL/company?apiKey=${process.env.API_KEY}`);
@@ -20,7 +19,7 @@ async function fetchStockData() {
 
 
     const prevClosePrice = prevCloseData.results && prevCloseData.results.length > 0 ? prevCloseData.results[0].c : null;
-    const currentPrice = currentPriceData.results && currentPriceData.results.length > 0 ? currentPriceData.results[0].c : null;
+   
     const symbol = infoData.symbol || null;
     const companyName = infoData.name || null;
 
