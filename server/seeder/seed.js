abe63fcefe6a2310fad75f01db7dd4de607f97cc
Jescel James <jesceljames@gmail.com>
2024-02-12 13:18:22 -0500

    update seeds to add cleanDB

--- server/seeder/seed.js
+++ server/seeder/seed.js
@@ -2,9 +2,13 @@ const db = require('../config/connection');
 const { User, Stock } = require('../models');
 const  userSeeds = require('./userSeeds.json');
 const stockSeeds = require('./stockSeeds.json')
+const cleanDB = require('./cleanDB')
 
 db.once('open', async () => {
   try {
+    await cleanDB('User', 'users');
+    await cleanDB('Stock', 'stocks');
+
     await User.create(userSeeds);
     await Stock.create(stockSeeds);
 
