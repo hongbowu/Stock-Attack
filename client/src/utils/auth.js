9f2df74040a09a9c40379bba65feb04ce928f658
hongbowu <wuhongbo87@gmail.com>
2024-02-13 15:20:33 -0500

    set loggedIn function

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -2,7 +2,26 @@ import decode from 'jwt-decode';
 
 class AuthService {
     getProfile() {
+        return decode(this.getToken());
+    }
 
+    loggedIn() {
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
+    }
+
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else return false;
+        } catch (err) {
+            return false;
+        }
+    }
+
+    getToken() {
     }
 }
 
