a4e7304c89ec4c85d72f270491e16a856dfcb125
hongbowu <wuhongbo87@gmail.com>
2024-02-13 10:39:06 -0500

    import all the pages and the router dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist'
 // import './index.css'
 //commented out because it affects the signup.jsx css
+
+import Dashboard from './pages/dashboard';
+import Homepage from './pages/homepage';
+import Login from './pages/login';
+import Signup from './pages/signup';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
