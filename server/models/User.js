4ca9ae4f926a97d8d82ae7285f3efa6de1e0c015
Jescel James <jesceljames@gmail.com>
2024-02-13 12:42:31 -0500

    User model with bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require ('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
 
@@ -20,7 +21,7 @@ const userSchema = new Schema({
 
   password: {
     type: String,
-    // required: true,
+    required: true,
     minlength: 5,
 
   },
@@ -34,7 +35,20 @@ const userSchema = new Schema({
 
 });
 
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
 
-  const User = model('User', userSchema);
+  next();
+});
 
-  module.exports = User;
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+
+const User = model('User', userSchema);
+
+module.exports = User;
