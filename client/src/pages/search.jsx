01553dedb4ca324350865b6080542172effe9731
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2024-02-13 12:21:33 -0500

    builds search page

--- client/src/pages/search.jsx
+++ client/src/pages/search.jsx
@@ -35,63 +35,21 @@ const Search = () => {
               alignItems: 'center',
             }}
           >
-            {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar> */}
             <img src={logo} alt="logo" />
-            <Typography component="h1" variant="h5">
-              Sign up
+            <Typography component="h1" variant="h3">
+              Search
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
               <Grid container spacing={2}>
                 {/* Form Fields */}
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete="given-name"
-                    name="firstName"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="family-name"
-                  />
-                </Grid>
                 <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControlLabel
-                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                    label="I want to receive updates via email."
-                  />
+                <TextField
+                  required
+                  fullWidth
+                  id="outlined-basic" 
+                  label="Stock Ticker" 
+                  variant="outlined" 
+                />
                 </Grid>
               </Grid>
               <Button
@@ -100,15 +58,8 @@ const Search = () => {
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
               >
-                Sign Up
+                Search Stock
               </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    Already have an account? Sign in
-                  </Link>
-                </Grid>
-              </Grid>
             </Box>
             <Typography
               variant="body2"
