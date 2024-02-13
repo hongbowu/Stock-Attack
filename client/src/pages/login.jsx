3abd2ff76d06255dea5c48aa97e48f33231527c5
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2024-02-13 15:38:18 -0500

    troubleshoots

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
+import { 
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Paper,
+  Box,
+  Grid,
+  Typography
+} from '@mui/material';
+// import { Button } from '@mui/material';
+// import { CssBaseline } from '@mui/material';
+// import { TextField } from '@mui/material';
+// import { FormControlLabel } from '@mui/material';
+// import { Checkbox } from '@mui/material';
+// import { Link } from '@mui/material';
+// import { Paper } from '@mui/material';
+// import { Box } from '@mui/material';
+// import { Grid } from '@mui/material';
+// import { Typography } from '@mui/material';
 import backgroundImage from '../images/login-background.png';
 import logo from '../assets/sa-logo-black.svg';
 import { useNavigate } from 'react-router-dom';
