e854d91b0a3e3879d215238b91e2fe83b993c16c
hongbowu <wuhongbo87@gmail.com>
2024-02-13 11:46:22 -0500

    remove the import we do not need

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,3 @@
-// import './App.css'
-// import Dashboard from './pages/dashboard';
-// import Homepage from './pages/homepage';
-// import Login from './pages/login';
-// import Signup from './pages/signup';
-// save the import for advance
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Outlet } from "react-router-dom";
 import Header from "./components/header";
