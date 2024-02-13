49b78a04311b0770bf636c70b31775e87b9eabe7
Brandon <brandondenzell566@outlook.com>
2024-02-13 07:03:23 -0500

    added the rest of the imports for mui

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -6,6 +6,11 @@ import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 const Signup = () => {
     return (
