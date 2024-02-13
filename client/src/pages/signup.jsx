f2f49ab67f20ed5d6c9bb836375fa31308821466
Brandon <brandondenzell566@outlook.com>
2024-02-13 06:58:29 -0500

    import baseline

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const Signup = () => {
     return (
