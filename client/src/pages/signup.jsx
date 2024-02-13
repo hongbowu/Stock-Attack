85b2723152e11f73da494cacbf98ff90cbc829c2
Brandon <brandondenzell566@outlook.com>
2024-02-13 06:59:00 -0500

    import text field

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
 
 const Signup = () => {
     return (
