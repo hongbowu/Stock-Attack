41bee3c2db1c6f48e9edf433892f579eedae8135
hongbowu <wuhongbo87@gmail.com>
2024-02-13 22:57:48 -0500

    add add user mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,14 @@
 import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
+        token
+        user {
+            _id
+            name
+        }
+    }
+  }
+`;
+
