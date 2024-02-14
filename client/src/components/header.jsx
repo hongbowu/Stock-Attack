c2cfab8cfd163b7b4c687fb8a60fb3d33c531086
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2024-02-14 13:51:26 -0500

    styles header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { CssBaseline, Box, Container, Typography, Paper } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import logo from '../../public/sa-logo.svg';
 
 const darkTheme = createTheme({
     palette: {
@@ -24,9 +25,10 @@ const Header = () => {
                             flexGrow: 1,
                             justifyContent: "start",
                             display: "flex",
-                            m:1
+                            m:1,
                         }}>
-                            <Typography variant="body2" color="text.secondary">
+                            <img src={logo} alt="logo" style={{ maxWidth: '2%', height: 'auto' }} />
+                            <Typography variant="body2" color="text.secondary" sx={{ px:2 }} >
                                 Stock Attack
                             </Typography>
                         </Box>
