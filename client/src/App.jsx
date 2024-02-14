76298e3e6b83dd38bd0cf1920576c08cec10450f
Jescel James <jesceljames@gmail.com>
2024-02-13 23:27:49 -0500

    add gql mutation LOGIN_USER, tested in graphql, works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
