c5cd40a865ae6211e0d984311a52bbf29503e321
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2024-02-15 10:35:36 -0500

    fixes links

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -163,7 +163,7 @@ const Signup = () => {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="login" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
